e5a002c77cac3b2a95fd86b6bf177f824bdf6f05
austin samsel <austinsamsel@gmail.com>
2016-08-03 00:02:05 -0400

    create-react-app && storybook && tests

---
+++ src/App.spec.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import { shallow } from 'enzyme';
+import chai, { expect } from 'chai';
+import chaiEnzyme from 'chai-enzyme';
+import { spy } from 'sinon';
+// import App from './App';
+
+chai.use(chaiEnzyme())
+
+describe('the environment', () => {
+  it('works, hopefully', () => {
+    expect(true).to.be.true;
+  });
+});
